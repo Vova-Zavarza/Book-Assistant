d54e242b0257e612d26ddf5247d1bc05aaa30970
Vova Zavarza <94126525+Vova-Zavarza@users.noreply.github.com>
2022-05-22 21:46:39 +0200

    Adaption Germany and visual changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import shelve
 
 
 class Book:
-    def __init__(self, name):
+    def __init__(self, name):           # Buchinformationen erstellen
         self.name = name
         self.pages = int(input('Dies ist ein neues Buch. Wie viel Seiten hat dieses Buch?: '))
         self.user_page = int(input('Wie viel Seiten hast du gelesen?: '))
@@ -16,15 +16,17 @@ class Book:
     def new_result(self):
         page = int(input('Auf welcher Seite bist du?: '))
         self.user_page = page
+        return f'Neuer Fortschrit ist {self.user_page}'
 
     def __str__(self):
-        return f"---Name: {self.name}\n---Seiten: {self.pages}\n---Dein Fortschirt: {self.user_page}"
+        return f"{'_' * 80}\n---Name: {self.name}\n---Seiten: {self.pages}\n---Dein Fortschirt: {self.user_page}\n{'_' * 80}"
 
 
 book_base = shelve.open(r'database/book_base') # open storage when save all information about books
 
-print("HELPER BOOK READER".center(32))
-user = input("Enter your book: ").rstrip()
+print("HELPER BUCHLESER".center(32))
+print(f"Deine Bibliotek hat: {list(book_base)}")
+user = input("Buchtitel eingeben: ").rstrip()
 
 if user in book_base:           # wenn dieses Buch alt ist
     real_book = book_base[user]
@@ -34,15 +36,15 @@ else:                           # wenn dieses Buch neu ist
 
 
 while True:                                                                                 # Hauptschliefe
-    command_dict = {"fortschrit":  real_book, "neuer fortschrit": real_book.new_result}
+    command_dict = {"fortschrit":  real_book, "neuer fortschrit": real_book.new_result, "tagesrate": real_book.tagesrate}
     command = input(">: ").lower().rstrip()
     if command == 'stop':
         book_base[real_book.name] = real_book
         book_base.close()
         break
     elif command in command_dict:
-        if command == 'neuer fortschrit':
-            command_dict[command]()
-        else:
+        if command == "fortschrit":
             print(command_dict[command])
-
+        else:
+            print(command_dict[command]())
+#test
